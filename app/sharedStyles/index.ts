295e2ab7789eeb4d6a347e64a176996cd0e8d7c4
Samuel Wemimo <wemsamuel@gmail.com>
2023-09-16 21:19:23 -0700

    New: clean

--- app/sharedStyles/index.ts
+++
@@ -1,5 +0,0 @@
-import { Colors, Font, Sizes, Shadows } from './theme';
-import sammyImage from '../assets/images/sammy.jpeg';
-// BKMRK: Im going to have to do images one at a time
-
-export { sammyImage, Colors, Font, Sizes, Shadows };
