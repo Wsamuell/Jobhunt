5a8670582a4b36f308eec4a3709724df6dce83e4
Samuel Wemimo <wemsamuel@gmail.com>
2023-09-23 13:15:06 -0700

    update: unsafe area display functional

--- App.tsx
+++ App.tsx
@@ -1,27 +1,78 @@
-import { Stack, useRouter } from 'expo-router';
+import { useRouter } from 'expo-router';
 import { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
 
 import { COLORS, icons, images, SIZES } from './constants';
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn } from './components';
 
-export default function App() {
-  const router = useRouter();
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: COLORS.lightWhite },
-          headerShadowVisible: false,
-        }}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: true }}
+      >
+        <Stack.Screen
+          name="Home"
+          options={() => ({
+            headerStyle: { backgroundColor: COLORS.lightWhite },
+            headerShadowVisible: false,
+            headerLeft: () => <ScreenHeaderBtn iconPath={icons.menu} />,
+            headerRight: () => <ScreenHeaderBtn picture={icons.menu} />,
+            title: '',
+          })}
+        >
+          {() => (
+            <Tab.Navigator
+              initialRouteName="Analitics"
+              tabBar={() => null}
+              screenOptions={{ headerShown: false }}
+            >
+              {/* <TouchableOpacity>
+                <Image source={{ uri: icons.menu }} style={styles.icon} />
+              </TouchableOpacity> */}
+              <Tab.Screen name="Analitics" component={ScreenHeaderBtn} />
+              <Tab.Screen name="Profile" component={ScreenHeaderBtn} />
+            </Tab.Navigator>
+          )}
+        </Stack.Screen>
+
+        <Stack.Screen name="Settings" component={ScreenHeaderBtn} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: COLORS.lightWhite,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    // justifyContent: 'space-between',
+    // alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingTop: 8,
+    paddingBottom: 8,
+    backgroundColor: COLORS.gray2,
   },
+  icon: {},
+  menu: {},
 });
+
+export default App;
--- components/common/header/ScreenHeaderBtn.tsx
+++ components/common/header/ScreenHeaderBtn.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-import styles from './screenheader.style'
+import styles from './screenheader.style';
+import { COLORS } from '../../../constants';
 
-const ScreenHeaderBtn = () => {
+type Prop = {
+  title?: string;
+  onPress?: () => void;
+  iconPath?: string;
+  picture?: string;
+};
+
+const ScreenHeaderBtn = ({ title, onPress, iconPath, picture }: Prop) => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
+    <View
+      style={
+        {
+          // flex: 1,
+          // backgroundColor: COLORS.lightWhite,
+          // justifyContent: 'space-between',
+          // alignItems: 'center',
+          // paddingTop: insets.top,
+          // paddingBottom: insets.bottom,
+          // paddingLeft: insets.left,
+          // paddingRight: insets.right,
+        }
+      }
+    >
+      {/* {title && <Text>{title}</Text>} */}
       <Text>ScreenHeaderBtn</Text>
+      {/* {picture && (
+        <Image
+          source={{ height: 24, width: 24, uri: picture }}
+          style={{ borderRadius: 12 }}
+        />
+      )} */}
+      {/* {iconPath && <Image source={{ height: 24, width: 24, uri: iconPath }} />} */}
     </View>
-  )
-}
+  );
+};
 
-export default ScreenHeaderBtn
+export default ScreenHeaderBtn;
