58073b30979be7efa345f64125c4769da084372b
Samuel Wemimo <wemsamuel@gmail.com>
2023-11-20 16:19:22 -0700

    new: covert to react native over expo and folder structure setup

--- constants/images.ts
+++
@@ -1,5 +0,0 @@
-import profile from '../assets/images/sam.jpeg';
-
-export default {
-  profile,
-};
--- constants/index.ts
+++
@@ -1,5 +0,0 @@
-import images from './images';
-import iconMap from './icons';
-import { COLORS, FONT, SIZES, SHADOWS } from './theme';
-
-export { images, iconMap, COLORS, FONT, SIZES, SHADOWS };
